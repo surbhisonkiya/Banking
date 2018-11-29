cb0a7191aa08ee341347b82c15813e7417436f5b
grvuolo93 <giovannirafael.vuolo@studenti.unitn.it>
2018-11-29 18:08:30 +0100

    delete classes

--- src/main/java/upm/ssa/app/OperationEnum.java
+++
@@ -1,9 +0,0 @@
-package main.java.upm.ssa.app;
-import java.io.Serializable;
-public enum OperationEnum implements Serializable{
-	CREATE_CLIENT,
-	READ_CLIENT,
-	UPDATE_CLIENT,
-	DELETE_CLIENT,
-	CREATE_BANK
-}
